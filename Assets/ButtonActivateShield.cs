9903575f7a30ab2c5069fd833f9c49ed58a0ccfa
ammirate <cesarano2607@gmail.com>
2015-09-30 13:48:04 +0200

    [ENHANCEMENT] Make Shield activatable in game by the user #6

---
+++ Assets/ButtonActivateShield.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//This script enables/disables the shield activator button
+public class ButtonActivateShield : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		checkShield ();
+	}
+
+
+	void Update() {
+		if (Input.GetMouseButtonDown(0)) {
+			if (Utility.checkInput(gameObject)) {
+				GameObject.FindGameObjectWithTag(GameTags.player).GetComponent<PlayerController>().activateShield();
+				gameObject.SetActive(false);
+			}
+		}
+	}
+
+	private void checkShield() {
+		if (!ShopList.getInstance ().getItem (ShopList.shield).isActivatable ()) {
+			gameObject.SetActive(false);
+		}
+	}
+}
--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -4,54 +4,55 @@ using System.Collections;
 
 
 public class PlayerController : MonoBehaviour {
-
+	//time of invulnerability after the shield destruction
 	private const float INVULNERABILITY_DEFAULT = 1f;
+	private static bool isInvulnerable = false;
+
+	private bool isShieldActive = false;
+	private bool blinking = false;
+	//material used for blink effect
+	public Material alternativeMaterial, originalMaterial;
 
 	public GameObject shield;
-	private bool isShieldActive = false;
 
-	private SpriteRenderer spriteRender;
+	private SpriteRenderer spriteRenderer;
 
-	private CoinsManager coins;
-	
-	private LevelManager level;
+	//Managers
+	private CoinsManager coinsManager;
+	private LevelManager levelManager;
+	private ScoreManager scoreManager;
+	private LifeManager lifeManager;
 
-	private ScoreManager score;
-
-	private LifeManager life;
-
+	//labels in game to update
 	public Text lifesText;
 	public Text coinsText;
 	public Text asteroidsText;
 
 	public GameObject deathAnimation;
-	
+
+	//audio stuff
 	public AudioClip point, death;
 	private AudioSource mainAudioSource;
 
-	private static bool isInvulnerable = false;
-	private SpriteRenderer spriteRenderer;
 
-	private bool blinking = false;
-	public Material alternativeMaterial, originalMaterial;
 
 	void Start() {
 		GameStatus.setPlayerAlive (true);
 		spriteRenderer = GetComponent<SpriteRenderer> ();
 
-		coins = CoinsManager.getInstance ();
-		level = LevelManager.getInstance ();
-		score = ScoreManager.getInstance ();
-		life = LifeManager.getInstance ();
-		score.resetScore ();
+		coinsManager = CoinsManager.getInstance ();
+		levelManager = LevelManager.getInstance ();
+		scoreManager = ScoreManager.getInstance ();
+		lifeManager = LifeManager.getInstance ();
+		scoreManager.resetScore ();
 
-		lifesText.text = "x " + life.getLifes();
+		lifesText.text = "x " + lifeManager.getLifes();
 		coinsText.text = "0";
 
 		mainAudioSource = GetComponents<AudioSource> ()[0];
 		mainAudioSource.volume = 0.1f;
 
-		spriteRender = GetComponent<SpriteRenderer> ();
+		spriteRenderer = GetComponent<SpriteRenderer> ();
 
 		checkShield ();
 
@@ -65,6 +66,7 @@ public class PlayerController : MonoBehaviour {
 			StartCoroutine(blink ());
 		}
 	}
+
 
 	void OnTriggerEnter2D(Collider2D coll) {
 //		Debug.Log("Collision entered with " + coll.gameObject);
@@ -95,10 +97,10 @@ public class PlayerController : MonoBehaviour {
 				}
 			}
 	}
-	
+
+
 	/*
 	 * Call the dead method of the player
-	 * 
 	 */
 	private void dead()	{
 		//add extra exp if a multiplier has been bought
@@ -106,19 +108,18 @@ public class PlayerController : MonoBehaviour {
 		//Debug.LogError ("You scored " + score.getScore() + "  and now EXTRA EXP adds " + level.calcExtraExp (score.getScore()));
 		GameStatus.setPlayerAlive (false);
 
-		level.increaseExp (level.calcExtraExp (score.getScore())); 
+		levelManager.increaseExp (levelManager.calcExtraExp (scoreManager.getScore())); 
 
-		coins.save ();
-		level.save ();
-		score.checkBestScore ();
-		score.save ();
+		coinsManager.save ();
+		levelManager.save ();
+		scoreManager.checkBestScore ();
+		scoreManager.save ();
 
 		loadRecap ();
 		deadAnimation ();
 	}
 
-
-
+	
 	private void deadAnimation() {
 		Instantiate (deathAnimation, transform.position, transform.rotation);
 		Destroy (gameObject);
@@ -133,35 +134,42 @@ public class PlayerController : MonoBehaviour {
 
 	void asteroidPassed(){
 		//Debug.Log("+1");
-		level.increaseExp ();
-		score.increaseScore ();
-		asteroidsText.text = "" + score.getScore ();
+		levelManager.increaseExp ();
+		scoreManager.increaseScore ();
+		asteroidsText.text = "" + scoreManager.getScore ();
 		Debug.Log ("actual score: " + ScoreManager.getInstance ().getScore ());
 	}
 
+
 	private void increaseCoins () {
 		Utility.playSoundOnSource (mainAudioSource, point, true, 0.3f);
-		coins.increaseCoins ();
-		score.increaseActualCoin ();
-		coinsText.text = "" + score.getActualCoins();
+		coinsManager.increaseCoins ();
+		scoreManager.increaseActualCoin ();
+		coinsText.text = "" + scoreManager.getActualCoins();
 
 	}
 
 
 	private void checkShield() {
 		if (ShopList.getInstance ().getItem (ShopList.shield).isActivatable ()) {
-			GameObject shieldObject = (GameObject) Instantiate(shield, transform.position, transform.rotation);
-			shieldObject.transform.SetParent(transform);
-
-			//put the shield on top of the player
-			Vector3 pos = shieldObject.transform.position;
-			pos.z = -2;
-			pos.y -= 0.02f;
-			shieldObject.transform.position = pos;
-
-			isShieldActive = true;
+			//activateShield();
+			Debug.Log("Can activate the shield");
 		}
 	}
+
+	public void activateShield() {
+		GameObject shieldObject = (GameObject) Instantiate(shield, transform.position, transform.rotation);
+		shieldObject.transform.SetParent(transform);
+		
+		//put the shield on top of the player
+		Vector3 pos = shieldObject.transform.position;
+		pos.z = -2;
+		pos.y -= 0.02f;
+		shieldObject.transform.position = pos;
+		
+		isShieldActive = true;
+	}
+
 
 	public IEnumerator setInvulnerable(float sec) {
 		isInvulnerable = true;
@@ -173,10 +181,10 @@ public class PlayerController : MonoBehaviour {
 	private IEnumerator blink() {
 		while (isInvulnerable) {
 			//spriteRenderer.color = Color.blue;
-			spriteRender.material = alternativeMaterial;
+			spriteRenderer.material = alternativeMaterial;
 			yield return new WaitForSeconds (0.15f);
 			//spriteRenderer.color = Color.white;
-			spriteRender.material = originalMaterial;
+			spriteRenderer.material = originalMaterial;
 			yield return new WaitForSeconds (0.15f);
 		}
 	}
